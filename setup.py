2cd6e876c43aac573d863dc6c0187eb71a3a85ca
Andrew Svetlov <andrew.svetlov@gmail.com>
2012-07-19 13:29:46 +0300

    add simple setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='steward',
+    version='0.0.2',
+    author='Andrew Svetlov',
+    author_email='andrew.svetlov@gmail.com',
+    url='https://github.com/asvetlov/steward',
+    packages=['steward'],
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    description='''Library for easy converting between plain JSON-like data and
+                 compound structure of user defined class instances.''',
+    long_description="""
+    """,
+)
